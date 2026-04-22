b4eb98b8cbbf2554823593b73709acbc2500aea7
Julieta Ramos <jramos@gruposimpli.com>
2026-04-22 12:29:48 -0300

    feat: update hello world output message

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,6 @@
 def main():
-    print("Hello World!")
+    print("Hello World!!!!!!!")
+    print("This is a simple Python program that prints 'Hello World' to the console.")
 
 if __name__ == "__main__":
     main()
